ce1ef66377909ead74c04dfcf9937793f8030a39
Sophie Price <64094672+SophiePrice99@users.noreply.github.com>
2020-06-10 16:31:21 +0100

    First Commit

---
+++ js/java.js
@@ -0,0 +1,38 @@
+function submitform() {
+  var $form = $('form')[0];
+  if ($form.checkValidity()) {
+    console.log('valid');
+    $form.submit();
+  } else {
+    console.log('not valid');
+  }
+  return false;
+};
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
